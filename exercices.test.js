89189a8e1c86c2994d223bb24e38991d56274acc
alfeugds <alfeugds@gmail.com>
2019-10-20 01:27:49 -0300

    initial commit

---
+++ exercices.test.js
@@ -0,0 +1,11 @@
+describe('Largest prime factor', () => {
+  /*
+  The prime factors of 13195 are 5, 7, 13 and 29.
+  What is the largest prime factor of the number 600851475143 ?
+  */
+  it('3 - largest prime factor of the number 600851475143 ', () => {
+    const isPrime = (n) => {
+      return n
+    }
+  })
+})
